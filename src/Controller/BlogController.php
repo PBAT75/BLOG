ff2e4645e4fe6aba8d24f019107d78cdda54b45f
Patricia Batjom <patriciabatjom@gmail.com>
2018-11-12 11:42:28 +0100

    modif gitignore

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * Show all row from article's entity
+     *
+     * @Route("/", name="blog_index")
+     * @return Response A response instance
+     */
+    public function index() : Response
+    {
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+        if (!$articles) {
+            throw $this->createNotFoundException(
+                'No article found in article\'s table.'
+            );
+        }
+
+        return $this->render(
+            'blog/index.html.twig',
+            ['articles' => $articles,
+                'controller_name' => 'BlogController']
+        );
+    }
+
+    /**
+     * @Route ("blog/{slug}", requirements={"slug"="\b[a-z0-9-]+\b"}, name="blog_show")
+     */
+    public function show($slug='article-sans-titre'):Response
+    {
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find an article in article\'s table.');
+        }
+
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'No article with '.$slug.' title, found in article\'s table.'
+            );
+        }
+        return $this->render(
+            'blog/show.html.twig',
+            [
+                'article' => $article
+            ]
+        );
+    }
+}
